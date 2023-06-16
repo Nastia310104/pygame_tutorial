0ddb3017fdc59ed1963bc3455157d40e439f24e3
Anastasiia Kotliar <somk1421@gmail.com>
2023-06-15 23:23:14 -0400

    Player controller added

---
+++ Controllers/PlayerController.py
@@ -0,0 +1,47 @@
+import pygame
+
+from Classes import Projectile
+from Controllers import ProjectileController
+
+def start(player, keys, bullets):
+    move(player, keys)
+    jump(player, keys)
+    shoot(player, keys, bullets)
+
+def move(player, keys):
+    if keys[pygame.K_LEFT] and player.x > player.vel:
+        player.move("left")
+    elif keys[pygame.K_RIGHT] and player.x < 500 - player.width - player.vel:
+        player.move("right")
+    else:
+        player.move("")
+
+def jump(player, keys):
+    if not(player.isJump):
+        if keys[pygame.K_UP]:
+            player.isJump = True
+            player.move("")
+    else:
+        if player.jumpCount >= -10:
+            negotive = 1
+            if player.jumpCount < 0:
+                negotive = -1
+            player.y -= int((player.jumpCount ** 2) * 0.5 * negotive)
+            player.jumpCount -= 1
+        else:
+            player.isJump = False
+            player.jumpCount = 10
+
+    
+
+def shoot(player, keys, bullets):
+    ProjectileController.bulletShoot(bullets)
+
+    if keys[pygame.K_SPACE]:
+        if player.left: 
+            facind = -1
+        else:
+            facind = 1
+
+        if len(bullets) < 5:
+            bullets.append(Projectile.Projectile(round(player.x + player.width // 2), round(player.y + player.height // 2), 5, (0, 0, 255), facind))
--- main.py
+++ main.py
@@ -2,17 +2,22 @@ import pygame
 pygame.init()
 
 import sys
-sys.path.append("Classes")
+sys.path += ["Classes", "Controllers"]
 
 from Classes import Player, Enemy, Projectile
+from Controllers import PlayerController, EnemyController
 
 window = pygame.display.set_mode((500, 480))
-
 pygame.display.set_caption("Tutorial game")
 
 bg = pygame.image.load('Images/background.jpg')
-
 clock = pygame.time.Clock()
+
+player = Player.Player(200, 410, 64, 64, 5, 27)
+goblin = Enemy.Enemy(100, 410, 64, 64, 3, 33, 450)
+bullets = []
+
+run = True
 
 def redrawGameWindow():
     window.blit(bg, (0, 0))
@@ -24,12 +29,6 @@ def redrawGameWindow():
 
     pygame.display.update()
 
-player = Player.Player(200, 410, 64, 64, 5, 27)
-goblin = Enemy.Enemy(100, 410, 64, 64, 3, 33, 450)
-bullets = []
-
-run = True
-
 while run:
     clock.tick(27)
 
@@ -37,44 +36,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-    for bullet in bullets:
-        if bullet.x < 500 and bullet.x > 0:
-            bullet.x += bullet.vel
-        else:
-            bullets.pop(bullets.index(bullet))
-
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_SPACE]:
-        if player.left: 
-            facind = -1
-        else:
-            facind = 1
-
-        if len(bullets) < 5:
-            bullets.append(Projectile.Projectile(round(player.x + player.width // 2), round(player.y + player.height // 2), 5, (0, 0, 255), facind))
-
-    if keys[pygame.K_LEFT] and player.x > player.vel:
-        player.move("left")
-    elif keys[pygame.K_RIGHT] and player.x < 500 - player.width - player.vel:
-        player.move("right")
-    else:
-        player.move("")
-
-    if not(player.isJump):
-        if keys[pygame.K_UP]:
-            player.isJump = True
-            player.move("")
-    else:
-        if player.jumpCount >= -10:
-            negotive = 1
-            if player.jumpCount < 0:
-                negotive = -1
-            player.y -= int((player.jumpCount ** 2) * 0.5 * negotive)
-            player.jumpCount -= 1
-        else:
-            player.isJump = False
-            player.jumpCount = 10
+    PlayerController.start(player, keys, bullets)
 
     redrawGameWindow()
 
