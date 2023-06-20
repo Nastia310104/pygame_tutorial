97a99e757bcf4f689d5c719a79e4583b7fd51178
Anastasiia Kotliar <somk1421@gmail.com>
2023-06-20 12:35:48 -0400

    Clean this

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 sys.path += ["Classes", "Controllers"]
 
 from Classes import Player, Enemy, Projectile
-from Controllers import PlayerController, EnemyController
+from Controllers import PlayerController, EnemyController, ProjectileController
 
 window = pygame.display.set_mode((500, 480))
 pygame.display.set_caption("Tutorial game")
@@ -13,8 +13,9 @@ pygame.display.set_caption("Tutorial game")
 bg = pygame.image.load('Images/background.jpg')
 clock = pygame.time.Clock()
 
-player = Player.Player(200, 410, 64, 64, 5, 27)
+player = Player.Player(100, 410, 64, 64, 5, 27)
 goblin = Enemy.Enemy(100, 410, 64, 64, 3, 33, 450)
+shootLoop = 0
 bullets = []
 
 run = True
@@ -31,6 +32,10 @@ def redrawGameWindow():
 
 while run:
     clock.tick(27)
+    if shootLoop > 0:
+        shootLoop += 1
+    if shootLoop > 3:
+        shootLoop = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -38,7 +43,14 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    PlayerController.start(player, keys, bullets)
+    PlayerController.start(player, keys)
+    ProjectileController.bulletShoot(bullets, goblin)
+
+
+    if keys[pygame.K_SPACE] and shootLoop == 0:
+        PlayerController.shoot(player, bullets)
+
+        shootLoop = 1
 
     redrawGameWindow()
 
